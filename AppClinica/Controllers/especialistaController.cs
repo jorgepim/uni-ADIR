3b664cf58536c43c70db95dc406c81948786a5b9
asensio01 <guillermo.asensio@catolica.edu.sv>
2025-05-24 04:18:27 -0600

    Validations

--- AppClinica/Controllers/especialistaController.cs
+++ AppClinica/Controllers/especialistaController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AppClinica.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
--- AppClinica/Models/Especialista.cs
+++ AppClinica/Models/Especialista.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace AppClinica.Models
 {
@@ -8,8 +9,12 @@ namespace AppClinica.Models
         public int IdUsuario { get; set; }
         public string Nombres { get; set; } = string.Empty;
         public string Apellidos { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression(@"^[A-Z0-9]{8,12}$", ErrorMessage = "El número JVPP debe tener entre 8 y 12 caracteres alfanuméricos, sin espacios.")]
         public string JVPP { get; set; } = string.Empty;
         public string Especialidad { get; set; } = string.Empty;
+
+        [RegularExpression(@"^\d{4}-\d{4}$", ErrorMessage = "El teléfono debe tener el formato 7777-7777")]
         public string Telefono { get; set; } = string.Empty;
         public string? Direccion { get; set; }
 
